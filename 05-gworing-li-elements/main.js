a2fa186ac1a8d3052978788eaec2a92709f9441a
KarolKarnas <karol.karnas@gmail.com>
2023-02-22 16:19:48 +0100

    Make every li font size lrger on click

--- 05-gworing-li-elements/main.js
+++ 05-gworing-li-elements/main.js
@@ -1,1 +1,15 @@
 // tutaj rozwiązanie
+let liFontSize = 10;
+
+const btn = document.querySelector('button');
+const liItems = document.querySelectorAll('li');
+
+const growLi = function () {
+	liFontSize += 2;
+	liItems.forEach((li) => {
+		li.style.display = 'block';
+		li.style.fontSize = liFontSize + 'px';
+	});
+};
+
+btn.addEventListener('click', growLi);
