3d1d7915ed3d78d614fa372e0e5cb78dc45b047f
KarolKarnas <karol.karnas@gmail.com>
2023-02-23 08:03:23 +0100

    Create 2 buttons with js
    1 button create and add to page 10 li elements with counter, every li has 1px larger font size
    2 reset button deletes all created li and reset counter and font size

--- 06-adding-and-growing/main.js
+++ 06-adding-and-growing/main.js
@@ -2,11 +2,46 @@ let size = 10;
 let orderElement = 1;
 
 const init = () => {
+const container = document.createElement('div');
+document.body.appendChild(container);
 
-}
+	const btn = document.createElement('button');
+	btn.textContent = 'Dodaj 10 elemenetnów do listy';
+	container.appendChild(btn);
+    
+    const resetBtn = document.createElement('button');
+    resetBtn.textContent = 'Reset'
+	container.appendChild(resetBtn);
+
+	const ul = document.createElement('ul');
+	document.body.appendChild(ul);
+
+	btn.addEventListener('click', createLiElements);
+	resetBtn.addEventListener('click', resetList);
+};
 
 const createLiElements = () => {
+	const ul = document.querySelector('ul');
+	for (let i = 0; i < 10; i++) {
+		const li = document.createElement('li');
+		li.textContent = `Element nr ${orderElement++}`;
+		li.style.fontSize = `${size++}px`;
+		ul.appendChild(li);
+     }
+};
 
+const resetList = () => {
+
+
+// const allLi = document.querySelectorAll('li');
+// allLi.forEach(el => {
+//     el.remove();
+// })
+
+document.querySelector('ul').textContent = "";
+
+orderElement = 1;
+size = 10;
 }
 
-init()
+init();
