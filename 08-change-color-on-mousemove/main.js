d3e7c15e02df4a4153d0441e16012b3696227a10
KarolKarnas <karol.karnas@gmail.com>
2023-02-23 09:20:18 +0100

    Change bg color on mouesemove
    Two exercise to change color on mouse move

--- 08-change-color-on-mousemove/main.js
+++ 08-change-color-on-mousemove/main.js
@@ -5,6 +5,7 @@ document.body.addEventListener('mousemove', function(e) {
   // h1.textContent = `${e.pageX}, ${e.pageY}`;
   h1.textContent = e.clientX + ", " + e.clientY;
 
+  document.body.style.backgroundColor = `rgb(${e.clientX/3}, ${e.clientY/2}, ${e.clientX/e.clientY*20})`
 
 })
 
---
+++ 09-change-color-on-mousemove/main.js
@@ -0,0 +1,24 @@
+const h1 = document.querySelector('h1');
+document.body.addEventListener('mousemove', function(e) {
+  // console.log(e.clientX, e.clientY);
+  // h1.textContent = `${e.screenX}, ${e.screenY}`;
+  // h1.textContent = `${e.pageX}, ${e.pageY}`;
+
+
+  const x = e.clientX;
+  const y = e.clientY;
+
+  const width = window.innerWidth
+  const height = window.innerHeight
+  h1.textContent = x + ", " + y;
+  
+  const red = x /width * 100;
+  const green = y / height * 100;
+  const blue = ((x /width * 100) + (y / height * 100)) / 2;
+  
+  console.log(x, y, width, height, `red: ${red}`, `green: ${green}`, `blue: ${blue}`);
+  document.body.style.backgroundColor = `rgb(${red}%, ${green}%, ${blue}%)`
+
+})
+
+
