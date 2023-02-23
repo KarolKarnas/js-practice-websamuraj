8ee1e4621a544d176790bb5ce34947a8e84e146e
KarolKarnas <karol.karnas@gmail.com>
2023-02-23 11:32:21 +0100

    Exercise 12 finish with switch

--- 12-body-brighter-darker/main.js
+++ 12-body-brighter-darker/main.js
@@ -1,21 +1,44 @@
 /* Gotowy projekt - sprzawdz działanie strzałek i zrób podobnie. http://websamuraj.pl/examples/js/projekt4-zad2/ */
 
-let red = 100;
-let green = 100;
-let blue = 100;
+let defaultColor = 100;
+let incrementation = 5;
 
-document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+let red = defaultColor;
+let green = defaultColor;
+let blue = defaultColor;
 
-const changeColor = (e) => {
-  console.log(e.keyCode, e.which);
-  // 38 - strzałka do góry
-  // 40 - strzalka w dół
-
-  // wciśnięcie klawisza strzałki do góry ma zmieniać kolor (stopniowo) na biały czyli rgb(255,255,255). Wciśniecie klawisza strzałki w dół ma zmienić kolor stopniowo na czarny (docelowo rgb(0,0,0)) 
-  //tutaj twój kod
-  // wersja 1 - instrukcja if
-  // wersja 2 - instrukcja switch
-
+const setColor = (val) => { 
+document.body.style.backgroundColor = `rgb(${val}, ${val}, ${val})`;
 }
 
-window.addEventListener('keydown', changeColor)
+setColor(defaultColor);
+
+const changeColor = (e) => {
+	// console.log(e.keyCode, e.which);
+	// 38 - strzałka do góry
+	// 40 - strzalka w dół
+
+	switch (e.keyCode) {
+		case 38:
+      defaultColor += incrementation;
+      defaultColor = Math.min(defaultColor, 255)
+			console.log(defaultColor);
+      setColor(defaultColor)
+			break;
+		case 40:
+      defaultColor -= incrementation;
+      defaultColor = Math.max(defaultColor, 0)
+			console.log(defaultColor);
+      setColor(defaultColor)
+		// default:
+		// 	console.log(e.keyCode);
+		// 	break;
+	}
+
+	// wciśnięcie klawisza strzałki do góry ma zmieniać kolor (stopniowo) na biały czyli rgb(255,255,255). Wciśniecie klawisza strzałki w dół ma zmienić kolor stopniowo na czarny (docelowo rgb(0,0,0))
+	//tutaj twój kod
+	// wersja 1 - instrukcja if
+	// wersja 2 - instrukcja switch
+};
+
+window.addEventListener('keydown', changeColor);
